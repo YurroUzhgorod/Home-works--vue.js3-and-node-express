976b30caa53d8b2e13d8e8d692d4b3d217e7326d
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2022-04-26 18:16:15 +0300

    add messenger from task2

---
+++ task2-after lesson-6/task-messenger/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <mesenger-component
+    :messages-list="messagesData"
+    @addMessage="addMessage"
+    @addLikes="addLikes"
+    @addDisLikes="addDisLikes"
+  />
+</template>
+
+<script>
+import MesengerComponent from "./components/MesengerComponent";
+
+export default {
+  name: "App",
+  components: {
+    MesengerComponent,
+  },
+  data() {
+    return {
+      messagesData: [],
+    };
+  },
+  methods: {
+    addMessage(newtext) {
+      this.messagesData.push({
+        text: newtext,
+        likes: 0,
+        dislikes: 0,
+      });
+    },
+    addLikes(searchMessage) {
+      searchMessage.likes += 1;
+    },
+    addDisLikes(searchMessage) {
+      searchMessage.dislikes += 1;
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ task2-after lesson-6/task-messenger/src/components/MesengerComponent/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div v-for="(message, index) in messagesList" :key="index">
+      {{ message.text }}
+      <button @click="$emit('addLikes', message)">like</button
+      ><button @click="$emit('addDisLikes', message)">dislike</button>Likes:{{
+        message.likes
+      }}
+      -- Dislikes:{{ message.dislikes }}
+    </div>
+    <label>
+      <input
+        type="text"
+        placeholder="type a new message"
+        v-model="currentMessage"
+      />
+    </label>
+    <button @click="$emit('addMessage', this.currentMessage)">send</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MesengerComponent",
+
+  props: {
+    messagesList: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      currentMessage: null,
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

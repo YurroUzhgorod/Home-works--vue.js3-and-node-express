1647facd84c9aefb38b8718700d1cd199d3ce33f
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2022-04-27 12:26:57 +0300

    decided the first task

--- 3---lesson7-task1/home-work/src/App.vue
+++ 3---lesson7-task1/home-work/src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <price-summator />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PriceSummator from "./components/PriceSummator";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    PriceSummator,
+  },
+};
 </script>
 
 <style>
--- 3---lesson7-task1/home-work/src/components/PriceSummator/ProductPriceEditor/index.vue
+++ 3---lesson7-task1/home-work/src/components/PriceSummator/ProductPriceEditor/index.vue
@@ -1,11 +1,29 @@
 <template>
-  <div></div>
+  <div>
+    <label>
+      {{ productName
+      }}<input
+        type="number"
+        @input="$emit('update:productPrice', $event.target.value)"
+      />
+    </label>
+  </div>
 </template>
 
 <script>
 export default {
   name: "ProductPriceEditor",
+  props: {
+    productName: {
+      type: Text,
+      required: true,
+    },
+    productPrice: {
+      type: Number,
+      default: null,
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="css" scoped></style>

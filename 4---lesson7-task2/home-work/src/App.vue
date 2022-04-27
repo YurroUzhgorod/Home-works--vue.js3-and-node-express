d985d81219af96b1e8b4b8a66c77cda8ef19e8ab
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2022-04-27 12:38:29 +0300

    fix first task from lesson 6

---
+++ 4---lesson7-task2/home-work/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <price-summator />
+</template>
+
+<script>
+import PriceSummator from "./components/PriceSummator";
+
+export default {
+  name: "App",
+  components: {
+    PriceSummator,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 4---lesson7-task2/home-work/src/components/PriceSummator/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="wrapper">
+    <product-price-editor
+      product-name="Phone"
+      v-model:product-price="productOnePrice"
+    />
+    <product-price-editor
+      product-name="Bicycle"
+      v-model:product-price="productTwoPrice"
+    />
+    <div class="result_block">
+      Total:
+      <span
+        v-if="this.productOnePrice !== null && this.productTwoPrice !== null"
+      >
+        {{ totalSum }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import ProductPriceEditor from "./ProductPriceEditor";
+export default {
+  name: "PriceSummator",
+  components: {
+    ProductPriceEditor,
+  },
+  data() {
+    return {
+      productOnePrice: null,
+      productTwoPrice: null,
+    };
+  },
+  computed: {
+    totalSum() {
+      return (
+        parseFloat(this.productOnePrice) + parseFloat(this.productTwoPrice)
+      );
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.wrapper {
+  width: 500px;
+  border: 2px red solid;
+  padding: 10px;
+
+  margin: auto;
+}
+
+.result_block {
+  margin: 20px;
+}
+</style>

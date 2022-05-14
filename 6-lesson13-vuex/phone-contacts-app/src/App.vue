be3e385622aa94414b23655940e03e3fdc4f93bc
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2022-05-14 14:52:24 +0300

    add task(vuex)

---
+++ 6-lesson13-vuex/phone-contacts-app/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div>
+      <!-- use the router-link component for navigation. -->
+      <!-- specify the link by passing the `to` prop. -->
+      <!-- `<router-link>` will render an `<a>` tag with the correct `href` attribute -->
+      <router-link to="/">Go to Home</router-link>
+      <router-link to="/edit">Go to Edit</router-link>
+    </div>
+    <!-- route outlet -->
+    <!-- component matched by the route will render here -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>

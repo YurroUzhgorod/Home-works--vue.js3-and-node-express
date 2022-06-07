04ccbf2a09d502be435a098d28aec794db630be5
Yuriy Kuliaba <yuriykuliaba@gmail.com>
2022-06-07 15:36:45 +0300

    added redirect route in my task

--- nodeJS-express/8-lesson20/start-express-app/routes/info.js
+++ nodeJS-express/8-lesson20/start-express-app/routes/info.js
@@ -4,29 +4,29 @@ var router = express.Router();
 
 /* GET users listing. */
 
-router.get("/:myLinks", function (req, res, next) {
-  if (req.params.myLinks === "about-me") {
-    res.render("about-me", {
-      title: "About-Yura-page",
-      name: "Yura",
-    });
-  }
+router.get("/", function (req, res, next) {
+  res.redirect("/info/about-me");
+}),
+  router.get("/:myLinks", function (req, res, next) {
+    if (req.params.myLinks === "about-me") {
+      res.render("about-me", {
+        title: "About-Yura-page",
+        name: "Yura",
+      });
+    }
 
-  if (req.params.myLinks === "favorite-cinemas") {
-    res.render("cinemas", {
-      title: "Cinemas pages",
-      cinemas: data.cinemas,
-    });
-  }
+    if (req.params.myLinks === "favorite-cinemas") {
+      res.render("cinemas", {
+        title: "Cinemas pages",
+        cinemas: data.cinemas,
+      });
+    }
 
-  if (req.params.myLinks === "favorite-web-sites") {
-    res.render("web-sites", {
-      title: "favorite-web-sites pages",
-      webSites: data.webSites,
-    });
-  }
-
-  // if (!req.params.myLinks){
-  //   res.render("error")
-});
+    if (req.params.myLinks === "favorite-web-sites") {
+      res.render("web-sites", {
+        title: "favorite-web-sites pages",
+        webSites: data.webSites,
+      });
+    }
+  });
 module.exports = router;
